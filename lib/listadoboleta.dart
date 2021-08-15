f6994d15b65c26c4aaf20430d7d4fabef9998277
Henry Eddy Choque Zacarias <henryeddyxdf@outlook.com>
2021-08-15 02:17:30 -0400

    eddy

--- lib/listadoboleta.dart
+++ lib/listadoboleta.dart
@@ -1,24 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:rrhh/services/boletas_service.dart';
 
-import 'Drawer/widget_drawer.dart';
+import 'package:rrhh/Drawer/widget_drawer.dart';
+import 'package:rrhh/models/boletas.dart';
+
 class BoletaVista extends StatefulWidget {
-  
-
   @override
   _BoletaVistaState createState() => _BoletaVistaState();
 }
 
 class _BoletaVistaState extends State<BoletaVista> {
   @override
+  void didUpdateWidget(BoletaVista oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    setState(() {});
+  }
+
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+        child: Scaffold(
       appBar: AppBar(
-        title: Text('Listado de Solicitudes Realizadas'),
+        title: Text('Mi Boleta'),
       ),
       drawer: MenuLateral(),
-    )
-    
-    );
-
+      body: FutureBuilder<List<Boleta>>(
+        future: BoletasService().getBoletas(),
+        builder: (BuildContext context, AsyncSnapshot<List<Boleta>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context, int index) {
+                Boleta item = snapshot.data![index];
+                return Dismissible(
+                  key: UniqueKey(),
+                  background: Container(
+                    color: Colors.red,
+                  ),
+                  onDismissed: (DismissDirection direction) {
+                    // ClientDatabaseProvider.db.deleteClientWithId(item.id);
+                  },
+                  child: ListTile(
+                    title: Text(item.nombres),
+                    subtitle: Text(item.liquidoPagable),
+                    leading: CircleAvatar(
+                      child: Text(item.cargo),
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //     builder: (BuildContext context) => AddEditClientPage(
+                      //       true,
+                      //       client: item,
+                      //     ),
+                      //   ),
+                      // );
+                    },
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    ));
   }
-  }
+}
--- lib/services/boletas_service.dart
+++ lib/services/boletas_service.dart
@@ -1,13 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'package:rrhh/models/boletas.dart';
 
-
 class BoletasService {
-  Future<List<Boleta>> getBoletas() async {
-    final url = 'https://getdataproject.com/rrhh/public/api/boletas?empleado_id=124';
+  Future<List<Boleta>>? getBoletas() async {
+    final url =
+        'https://getdataproject.com/rrhh/public/api/boletas?empleado_id=124';
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200 || response.statusCode == 201) {
-      final boletasresponse= boletasDatosFromMap(response.body);
+      final boletasresponse = boletasDatosFromMap(response.body);
       return boletasresponse.boletas;
     }
     return [];
